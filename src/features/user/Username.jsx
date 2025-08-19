6d3f5b1b2492789ada7c1f349f2c2e7c504f4fe9
Sohail Shaikh <sohail28195@gmail.com>
2025-08-19 19:08:31 +0530

    added some more styling :lipstick:

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,7 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Sohail Shaikh</div>;
+  return (
+    <div className="hidden text-sm font-semibold md:block">Sohail Shaikh</div>
+  );
 }
 
 export default Username;
